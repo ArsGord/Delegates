44782356f9af5b2a239888f0dc91b88a5d004a3e
ArsGord <67157452+ArsGord@users.noreply.github.com>
2020-12-30 21:07:18 +0400

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        delegate void Calculate(int a, int b, ref double result); // Объявляем делегат
+        static void Main(string[] args)
+        {
+            double c = 0;
+            Calculate calculate;
+            calculate = Sub; // Добавляем методы
+            calculate += Add;
+            calculate += Div;
+            calculate += Mul;
+            calculate(56, 48, ref c); // Вводим значения
+            Console.WriteLine();
+            calculate -= Div; // Убираем деление
+            calculate(46, 10, ref c);
+            Console.ReadKey();
+        }
+        private static void Sub (int a, int b, ref double result)
+        {
+            Console.WriteLine(result);
+            result = a - b;
+            Console.WriteLine($"Результат вычитания: {result}");
+        }
+        private static void Add (int a, int b, ref double result)
+        {
+            Console.WriteLine(result);
+            result = a + b;
+            Console.WriteLine($"Результат сложения: {result}");
+        }
+        private static void Div (int a, int b, ref double result)
+        {
+            Console.WriteLine(result);
+            result = a / b;
+            Console.WriteLine($"Результат деления: {result}");
+        }
+        private static void Mul (int a, int b, ref double result)
+        {
+            Console.WriteLine(result);
+            result = a * b;
+            Console.WriteLine($"Результат умножения: {result}");
+        }
+    }
+}
